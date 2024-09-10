877c4d64fca3e7018f8a1c3a785eefa34e1fefd8
yechan <83994550+yechan-9208@users.noreply.github.com>
2024-09-10 20:04:15 +0800

    [level 0] Title: 문자열 겹쳐쓰기, Time: 20.00 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.kt
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.kt
@@ -1,20 +1,9 @@
 class Solution {
     fun solution(my_string: String, overwrite_string: String, s: Int): String {
-        var answer: String = ""
+        var length = overwrite_string.length
+        var newString = my_string.substring(0,s)+overwrite_string+my_string.substring(s+length)
         
-        for(i in 0..s-1){
-            answer+=my_string[i]
-        }
         
-        for(c in overwrite_string){
-            answer+=c
-        }
-        
-        for(i in overwrite_string.length+s..my_string.length-1){
-            answer+=my_string[i]
-        }
-       	
-        
-        return answer
+        return newString
     }
 }
