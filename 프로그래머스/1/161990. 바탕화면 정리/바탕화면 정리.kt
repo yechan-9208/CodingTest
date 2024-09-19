322564acb60e4412e9e0da59e16a6206d650788a
yechan <83994550+yechan-9208@users.noreply.github.com>
2024-09-19 21:35:09 +0800

    [level 1] Title: 바탕화면 정리, Time: 12.52 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.kt
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.kt
@@ -1,24 +1,6 @@
 class Solution {
     fun solution(wallpaper: Array<String>): IntArray {
-        
-        var x : Int
-        var y : Int
-        
-        var arrX : MutableList<Int> = mutableListOf()
-        var arrY : MutableList<Int> = mutableListOf()
-        
-        for(x in 0 until wallpaper.size){
-            for(y in 0 until wallpaper[x].length){
-                if(wallpaper[x][y]=='#'){
-                    arrX.add(x)
-                    arrY.add(y)
-                }       
-            }
-        }
-
-        arrX.sort()
-        arrY.sort()
-        
-        return intArrayOf(arrX.first(),arrY.first(),arrX.last()+1,arrY.last()+1)
+        var answer: IntArray = intArrayOf()
+        return answer
     }
 }
